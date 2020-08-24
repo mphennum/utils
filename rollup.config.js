ae9a81c91cbb981a850cf5d593de08233be20925
Max Hennum <mphennum@gmail.com>
2020-08-23 21:26:30 -0700

    rollup with preserved modules

--- rollup.config.js
+++ rollup.config.js
@@ -5,9 +5,11 @@ const PROD = (process.env.BUILD === 'production');
 export default {
 	input: 'src/index.js',
 	output: {
-		file: 'dist/index.js',
+		dir: 'dist',
+		// file: 'dist/index.js',
 		format: 'cjs',
-		// format: 'umd',
+		// format: 'iife',
+		preserveModules: true,
 	},
 	plugins: [
 		nodeResolve(),
