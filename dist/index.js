8cf65e4bcbdcf9548fa956bc03e8504f26bc1b56
Max Hennum <mphennum@gmail.com>
2020-08-23 21:49:42 -0700

    build

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,1 @@
-'use strict';Object.defineProperty(exports,'__esModule',{value:true});var index=require('./array/index.js'),index$1=require('./string/index.js');let v = { };
-let test = v?.test ?? 1;exports.array=index;exports.string=index$1;exports.test=test;
+'use strict';Object.defineProperty(exports,'__esModule',{value:true});var index=require('./array/index.js'),index$1=require('./string/index.js');exports.array=index;exports.string=index$1;
