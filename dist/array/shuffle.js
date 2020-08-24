a6487d9e6c9c774e63d792e4888ae6b79af3ded9
Max Hennum <mphennum@gmail.com>
2020-08-23 21:42:51 -0700

    build

---
+++ dist/array/shuffle.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let shuffle = (arr) => {
+    let cur = arr.length;
+
+    while (cur) {
+        let rand = Math.floor(Math.random() * cur);
+        cur--;
+
+        let tmp = arr[cur];
+        arr[cur] = arr[rand];
+        arr[rand] = tmp;
+    }
+
+    return arr;
+};
+
+module.exports = shuffle;
---
+++ dist/string/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var genId = require('./gen-id.js');
+var randId = require('./rand-id.js');
+
+let string = {
+	genId,
+	randId,
+};
+
+module.exports = string;
