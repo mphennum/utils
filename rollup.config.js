f41a77ac1ecc1e62c951a4c45f950b5d21d9eb9b
Max Hennum <mphennum@gmail.com>
2020-09-05 17:07:16 -0700

    simplified rollup cfg

--- rollup.config.js
+++ rollup.config.js
@@ -2,26 +2,20 @@ import babel from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
-const IIFE = {
-	file: 'dist/utils.js',
-	format: 'iife',
-	name: 'utils',
-};
-
 export default {
 	input: 'src/index.js',
 	output: [
 		{
-			dir: 'dist',
-			// file: 'dist/index.js',
+			// dir: 'dist',
+			file: 'dist/index.js',
 			format: 'cjs',
-			preserveModules: true,
+			// preserveModules: true,
 			exports: 'auto',
 		},
-		IIFE,
 		{
-			...IIFE,
-			file: IIFE.file.replace(/\.js$/, '.min.js'),
+			file: 'dist/utils.js',
+			format: 'iife',
+			name: 'utils',
 			compact: true,
 			plugins: [ terser() ],
 		},
